b1472db8e1ba22e028e1e9b006a67e8cdc66d733
Glebovski <andrewglebovski@gmail.com>
2022-08-22 10:22:10 +0300

    Translate header
    Перевел на английский header.h

--- header.h
+++ header.h
@@ -1,28 +1,24 @@
 /**
  * \file
- * \brief Это основной заголовочный файл
+ * \brief This is a header file
  * 
- * Он используется во всем проекте и содержит функции, структуры, перечисления необходимые в разных областях проекта
- * 
- * \todo Мне не нравится, что в него включен <math.h>, который нужен для использования NAN и fabs(). Подключать такую огромную библиотеку ко всем файлам проекта ради двух макросов это плохо, на мой взгляд
+ * Included in every project script
 */
 
 #include <math.h>
 
 
-/// Описывает состояние решения
+/// Shows solution status
 enum SOLUTION_STATUS {
-    ERROR         = -1, ///< Найти корни не удалось из-за математической ошибки
-    NO_SOLUTIONS  =  0, ///< У этого уравнения нет корней
-    ONE_SOLUTION  =  1, ///< У этого уравнения один корень
-    TWO_SOLUTIONS =  2  ///< У этого уравнения два корня
+    ERROR         = -1, ///< Solutions could not be found cause of a math error
+    NO_SOLUTIONS  =  0, ///< This equation has no solutions.
+    ONE_SOLUTION  =  1, ///< This equation has one solution.
+    TWO_SOLUTIONS =  2  ///< This equation has two solutions.
 };
 
 
 /**
- * \brief Содержит корни уравнения
- * 
- * \todo Иногда мне хочется, чтобы вместо x1 и x2 был массив с неопределенным размером для переменного числа решений, но это будет трудно реализовать
+ * \brief Contains the solutions of the equation
 */
 typedef struct solution {
     int status;
@@ -32,35 +28,35 @@ typedef struct solution {
 
 
 /**
- * \brief Функция ввода
- * \warning В функции используется assert. Неправильный ввод завершит программу
- * \param a,b,c Указатели на double
+ * \brief Input function
+ * \warning The function uses assert(). Wrong input will terminate the program
+ * \param a,b,c Pointers to double
  * 
- * Эта функция обрабатывает ввод коэффициентов и осуществляет его проверку
+ * This function gets coefficients and checks them.
 */
 void input(double *a, double *b, double *c);
 
 
 /**
- * \brief Функция вывода
- * \param result Структура Solution
+ * \brief Output function
+ * \param result #Solution to print
  * 
- * Эта функция обрабатывает вывод результата вычисления
+ * This function prints the solution
 */
 void output(Solution result);
 
 
 /** 
- * \brief Решает квадратное уравнение 
- * \param a,b,c Коэффициенты квадратного уравнения
- * \return Решение в виде структуры Solution
+ * \brief Solves a quadratic equation
+ * \param a,b,c Coefficients of a quadratic equation
+ * \return Result as a #Solution structure
 */
 Solution solve_quadratic(double a, double b, double c);
 
 
 /** 
- * \brief Решает линейное уравнение 
- * \param k,b Коэффициенты линейного уравнения
- * \return Решение в виде структуры Solution
+ * \brief Solves a linear equation
+ * \param k,b Coefficients of a linear equation
+ * \return Result as a #Solution structure
 */
 Solution solve_linear(double k, double b);
